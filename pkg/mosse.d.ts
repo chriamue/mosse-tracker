eabb9dca0cec89a8afeb20dc7652e3887f09ad48
chriamue <chriamue@users.noreply.github.com>
2022-08-29 10:47:55 +0000

    deploy: 6526e35c1daafecf96628916f67237edcc3f0f9a

---
+++ pkg/mosse.d.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class MultiMosseTrackerJS {
+  free(): void;
+/**
+* @param {number} width
+* @param {number} height
+*/
+  constructor(width: number, height: number);
+/**
+* @param {number} x
+* @param {number} y
+* @param {Uint8Array} img_data
+*/
+  set_target(x: number, y: number, img_data: Uint8Array): void;
+/**
+* @param {Uint8Array} img_data
+* @returns {Uint8Array}
+*/
+  track(img_data: Uint8Array): Uint8Array;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_multimossetrackerjs_free: (a: number) => void;
+  readonly multimossetrackerjs_new: (a: number, b: number) => number;
+  readonly multimossetrackerjs_set_target: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly multimossetrackerjs_track: (a: number, b: number, c: number, d: number) => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/mosse_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_multimossetrackerjs_free(a: number): void;
+export function multimossetrackerjs_new(a: number, b: number): number;
+export function multimossetrackerjs_set_target(a: number, b: number, c: number, d: number, e: number): void;
+export function multimossetrackerjs_track(a: number, b: number, c: number, d: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
